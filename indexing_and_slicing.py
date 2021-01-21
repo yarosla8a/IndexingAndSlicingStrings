15394fda6d3ab842259ba5941ee4fb99fe4fa938
Yaroslava <33331868+yarosla8a@users.noreply.github.com>
2021-01-21 22:06:46 +0200

    Add files via upload

---
+++ indexing_and_slicing.py
@@ -0,0 +1,26 @@
+greeting ='Hello Python!'
+greeting_length=len(greeting)
+print(greeting_length)
+
+#Indexing
+print(greeting[0])
+print(greeting[6])
+print(greeting[-1])
+print(greeting[-4])
+
+#Slicing
+print(greeting[2:5])
+print(greeting[6:10])
+print(greeting[-5:-2])
+print(greeting[3:])
+print(greeting[:-5])
+print(greeting[::2])
+print(greeting[::3])
+print(greeting[1::3])
+print(greeting[1:9:3])
+
+
+print(greeting[::-1])
+
+
+
